df3a7d27b28002ea1c83b6b80fb4ddf69a1c6841
Rineesa <rinesa.berisha@studenti.unicam.it>
2024-02-12 18:42:14 +0100

    First touch down of the Category class for the supermarket class

---
+++ loyaltyPlatform/src/main/java/it/unicam/cs/ids/loyaltyPlatform/supermaketCashRegisterSimulator/Category.java
@@ -0,0 +1,15 @@
+package it.unicam.cs.ids.loyaltyPlatform.supermaketCashRegisterSimulator;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Category {
+
+
+}
